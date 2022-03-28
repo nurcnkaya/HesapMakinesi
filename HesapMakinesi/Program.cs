7b891c62575316e22a05786b638dff9f2620b0d5
Nurcan Kaya <102388954+nurcnkaya@users.noreply.github.com>
2022-03-28 11:06:45 +0300

    Add project files.

---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1_giris, sayi2_giris;
+            double sonuc;
+            string islem;
+
+            Console.WriteLine("Bir sayı girin:");
+            sayi1_giris = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("İkinci sayıyı girin:");
+            sayi2_giris = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İşlem seçin(*,/,+,-)");
+            islem = Console.ReadLine();
+
+            if (islem == "*")
+            {
+                sonuc = sayi1_giris * sayi2_giris;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            if (islem == "/")
+            {
+                sonuc = sayi1_giris / sayi2_giris;
+                Console.WriteLine("Sonuç:" + sonuc);
+            }
+            if (islem == "+")
+            {
+                sonuc = sayi1_giris + sayi2_giris;
+                Console.WriteLine("Sonuç:" + sonuc);
+            }
+            if (islem == "-")
+            {
+                sonuc = sayi1_giris - sayi2_giris;
+                Console.WriteLine("Sonuç:" + sonuc);
+            }
+        }
+
+
+        }
+    }
